d6f49ab95861359640a954c59907a0659a29df3b
keziahprecious4-byte <keziahprecious4@gmail.com>
2026-01-05 15:51:45 +0100

    New class
    Class Customer(Person)

--- project/models/customer.py
+++ project/models/customer.py
@@ -2,3 +2,16 @@ import csv
 from models.person import Person
 
 fileName = "customers.csv"
+class Customer(Person):
+    def __init__(self, name, age, email, customerId):
+        super().__init__(name)
+        self.age = age
+        self.email = email
+        self.customerId = customerId
+
+    def showInfo(self):
+        print(f"ID: {self.customerId}") 
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Email: {self.email}")
+        
