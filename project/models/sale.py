e124b8480676ad87475fcd69ff0e47ba0051d496
keziahprecious4-byte <keziahprecious4@gmail.com>
2026-01-07 14:22:50 +0100

    class Sales
    make purchase

--- project/models/sale.py
+++ project/models/sale.py
@@ -2,3 +2,20 @@ import csv
 
 fileName = "sales.csv"
 
+class Sales:
+    def __init__(self, event, customer, ticket):
+        self.evet = event
+        self.customer = customer
+        self.ticket = ticket
+
+    def makePurchase(self):
+        if not self.ticket.available:
+            print("Ticket not available.")
+            return False
+        
+        self.ticket.available = False
+        self.saveToFile()
+
+        print("Purchase successful.")
+        return True
+    
