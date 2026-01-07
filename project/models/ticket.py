af11db3650c1f68b62286cb3856df9dee2a7e203
keziahprecious4-byte <keziahprecious4@gmail.com>
2026-01-07 14:42:24 +0100

    Class Ticket
     __init__ and show info

--- project/models/ticket.py
+++ project/models/ticket.py
@@ -1,2 +1,18 @@
 import csv
 
+class Ticket:
+    def __init__(self, ticketId, ticketType, price, available=True)
+        self.ticketId = ticketId
+        self.ticketType = ticketType
+        self.price = price
+        self.available = available
+
+    def showInfo(self):
+        status = "Available" if self.available else "Sold"
+        print(f"ID: {self.ticketId}")
+        print(f"Type: {self.ticketType}")
+        print(f"Price: {self.price}")
+        print(f"Status: {status}")
+
+
+
