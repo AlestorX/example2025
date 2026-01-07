8796ff6e4cdc4f054df21526f94076b4c6997bf4
heeva1702 <shahraki.heeva@gmail.com>
2026-01-07 14:39:03 +0300

    Update event.py

---
+++ project/models/event.py
@@ -0,0 +1,11 @@
+from artist import Artist
+
+
+
+class Event:
+    def __int__(self, event_name, date, start_time, end_time, artists):
+        self.event_name = event_name
+        self.date = date
+        self.start_time = start_time
+
+        
