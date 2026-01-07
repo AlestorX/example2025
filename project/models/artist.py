861fc13ce56289ca887b691a917d0e14d23df424
heeva1702 <shahraki.heeva@gmail.com>
2026-01-07 16:53:31 +0300

    save

--- project/models/artist.py
+++ project/models/artist.py
@@ -47,8 +47,7 @@ def artist_menu():
 
 
         if choice == "1":
-            list_artists(artists)
-            return artists 
+            list_artists(artists) 
         elif choice == "0":
             print("Bye!")
             return artists
