5f79a390cc15e7675ff82e1abdf046815ae607cb
heeva1702 <shahraki.heeva@gmail.com>
2026-01-07 15:19:07 +0300

    save
    save

--- project/main.py
+++ project/main.py
@@ -1,11 +1,16 @@
 from models.artist import artist_menu
-
+from models.event import Event
 
 def main():
     artists = artist_menu()
     print("\nArtists received in main.py")
     for a in artists:
         a.show()
+
+    event1 = Event ("Summer Music Festival", "18.05.2026", "18:00", "00:00", artists)
+
+
+    event1.show_event()
 
 if __name__ == "__main__":
     main()
--- project/models/event.py
+++ project/models/event.py
@@ -1,19 +1,22 @@
-from artist import Artist
+from .artist import Artist
 
 
 
 class Event:
-    def __int__(self, event_name, date, start_time, end_time, artists):
+    def __init__(self, event_name, date, start_time, end_time, artists):
         self.event_name = event_name
         self.date = date
         self.start_time = start_time
+        self.end_time = end_time
 
         self.artists = artists 
 
-        def show_event(self):
+    def show_event(self):
             print("\n=== Event Info ===")
             print("Event:", self.event_name)
             print("Date:", self.date)
             print("Time:", self.start_time, "-", self.end_time)
 
-            
+            print("Artists:")
+            for a in self.artists:
+                a.show()
