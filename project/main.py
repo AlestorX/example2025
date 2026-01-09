e0a17845bed15f5dce2b4afaf34413223c7cfc5f
TesiSerg <taisias376@gmail.com>
2026-01-09 17:42:04 +0300

    save

--- project/main.py
+++ project/main.py
@@ -1,7 +1,23 @@
 from models.artist import artist_menu
 from models.event import Event
 
+
 def main():
     while True:
         print("\n=== MAIN MENU ===")
-        print("1)")
+        print("1) Artists")
+        print("0) Exit")
+
+        choice = input("Choose: ").strip()
+
+        if choice == "1":
+            artist_menu()
+        elif choice == "0":
+            print("Bye!")
+            break
+        else:
+            print("Something went wrongTry again or make another choice.")
+
+
+if __name__ == "__main__":
+    main()
