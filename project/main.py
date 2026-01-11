6c935220c806c9ad7a6418b7123f9c8a26a8354d
AlestorX <storalexstor@icloud.com>
2026-01-11 13:56:13 +0100

    fix

--- project/main.py
+++ project/main.py
@@ -1,23 +1,26 @@
 from models.artist import artist_menu
 from models.event import Event
 
-
 def main():
     while True:
         print("\n=== MAIN MENU ===")
         print("1) Artists")
+        print("2) Customers")
         print("0) Exit")
 
-        choice = input("Choose: ").strip()
+        choice = input("Choice: ").strip()
 
-        if choice == "1":
+        if choice =="1":
             artist_menu()
+        elif choice =="2":
+            customer_menu()
         elif choice == "0":
             print("Bye!")
             break
         else:
-            print("Something went wrongTry again or make another choice.")
+            print("Invalid choice.")
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
+
+    
