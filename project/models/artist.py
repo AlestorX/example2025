f509f50d5443ac5157cc7291da5bf84667d01351
heeva1702 <shahraki.heeva@gmail.com>
2026-01-13 12:40:17 +0300

    Update artist.py

--- project/models/artist.py
+++ project/models/artist.py
@@ -1,6 +1,7 @@
 from models.person import Person
 
-
+import os
+import csv
 
 class Artist: 
     def __init__(self, name, genre):
@@ -9,6 +10,32 @@ class Artist:
 
     def show(self):
         print(f"- {self.name} ({self.genre})")
+
+
+ARTISTS_FILE = "data/artists.csv"
+
+def load_artists():
+     # If file doesn't exist, return default artists
+     if not os.path.excits(ARTISTS_FILE):
+         return [
+             Artist("Shakira", "pop"),
+             Artist("the weeknd", "R&B"),
+             Artist("Rihanna", "pop")
+         ]
+     
+     artists = []
+     with open (ARTISTS_FILE, "r", newline="", encoding="utf-8") as f:
+         reader = csv.reader(f)
+         for row in reader:
+             if len(row) != 2:
+                 continue
+             name = row[0].strip()
+             genre = row[1].strip()
+             artists.append (Artist(name, genre))
+
+     return artists 
+
+
 
 def list_artists(artists):
     if len(artists) == 0:
